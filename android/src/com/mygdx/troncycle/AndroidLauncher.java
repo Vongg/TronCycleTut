e38d86a51382c135e923eed0ba2def6c671f1c7e
Vong <mateuszvg@gmail.com>
2016-08-11 20:34:20 +0200

    Setup

---
+++ android/src/com/mygdx/troncycle/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.troncycle;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.troncycle.TronCycle;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TronCycle(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/troncycle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.troncycle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.troncycle.TronCycle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TronCycle(), config);
+	}
+}
